e756aed7c13247e9e08b19222d674edfb62fb46e
Edfolmi <ephraimdanielfolmi@gmail.com>
2023-12-13 23:06:48 +0100

    code the header navbar

---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import '../css/style.css';
+import logo from '../logo192.png'
+
+
+const Header = (id) => {
+    return <header className='header'>
+        <div className="header-container">
+            <nav className="nav-bar">
+                <a href="/"><img src={logo} className="logo" alt="larjapay logo" /></a>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Login</li>
+                    <li>Logout</li>
+                    <li>Sign Up</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+}
+
+export default Header;
